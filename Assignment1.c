2318dad7448a03903769dc454c3807a0543a67f8
Lavkush <college.studying23@gmail.com>
2025-01-06 18:11:04 +0530

    Assigment 4 question

---
+++ Assignment1.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//count set bits in an integer
+int count_set_bit(int n) {
+    int count=0, counter;
+    counter = n;
+    int mask = 1;
+    while(counter != 0) {
+        if((n&mask) != 0) {
+            count++;
+        }
+        counter = counter>>1;
+        mask = mask<<1;
+    }
+    return count;
+}
+
+//program to find wheather number is power of two 
+void power_of_two(int n) {
+    if((n&(n-1)) == 0) {
+        printf("Yes");
+    } else {
+        printf("No");
+    }
+};
+
+//find position of set bit 
+void position_of_set_bit(int n) {
+    int counter = n,count=0;
+    int mask = 1;
+
+    printf("Positions of set bits: ");
+    while(counter != 0) {
+        if((n&mask) != 0) {
+            printf("%d ", count);
+        }
+        counter = counter>>1;
+        mask = mask << 1; 
+        count++;
+    }
+}
+
+//Count total set bits from number 1 to n
+void set_bit_one_to_n(int n) {
+    int count = 0;
+
+    for(int i=1; i<=n; i++) {
+        int c = count_set_bit(i);
+        count += c;
+    }
+    printf("%d set bits", count);
+}
+
+
+//find to non repeating elements in a array of repeating elements
+//count number of bit flipped to convert A to B
+//calculate square of a number without using /,* and pow()
+int square(int n) {
+
+}
+
+//divide a two integers without using * ,/ , and % mod operator
+
+//powers set
+
+
+int main() {
+    printf("%d\n",count_set_bit(11));
+    position_of_set_bit(11);
+    printf("\n");
+    power_of_two(16);
+    printf("\n");
+    set_bit_one_to_n(10);
+    printf("\n%d", divide(10,3));
+
+    // printf("%d", set_bit_one_to_n(0));
+
+    return 0;
+}
